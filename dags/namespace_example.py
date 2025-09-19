73bb93229e3029b29f6ddcd1616bb74c11ebc0ff
Atul Singh Rajpoot <rajpootatul02@gmail.com>
2025-09-19 17:10:14 +0530

    Create namespace_example.py

---
+++ dags/namespace_example.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2024, 1, 1)
+}
+
+with DAG('namespace_example', default_args=default_args, schedule_interval='@daily') as dag:
+    task = PythonOperator(
+        task_id='run_in_other_namespace',
+        python_callable=my_function,
+        executor_config={
+            "KubernetesExecutor": {
+                "namespace": "team-alpha"  # Specify your target namespace here
+            }
+        }
+    )
