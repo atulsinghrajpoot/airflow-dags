0e72abd47e6f171c8aafc74ffed11a1f4844e728
Atul Singh Rajpoot <rajpootatul02@gmail.com>
2025-09-19 18:25:52 +0530

    Create mulit_namespace.py

---
+++ dags/mulit_namespace.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2024, 1, 1)
+}
+def my_function():
+    print("Hello from the Airflow task!")
+ 
+with DAG( dag_id="namespace_example",
+    default_args=default_args,
+    description="A simple sample DAG",
+    schedule=timedelta(minutes=5),  # runs every 1 minute
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    tags=["example"]) as dag:
+    
+    task = PythonOperator(
+        task_id='run_in_other_namespace',
+        python_callable=my_function,
+        executor_config={
+            "pod_override": {
+                "metadata": {"namespace": "cisco"}
+            }
+        }
+    )
