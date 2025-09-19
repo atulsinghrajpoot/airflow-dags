5ca155d1deb2895c80dac4c02d9c50de2cd075c1
Atul Singh Rajpoot <rajpootatul02@gmail.com>
2025-09-19 18:58:34 +0530

    Update sample_dag.py

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -1,8 +1,8 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
+from kubernetes.client import V1Pod, V1ObjectMeta  # import required classes
 
-# Default args for the DAG
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
@@ -12,32 +12,33 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
-# Define DAG
 with DAG(
     dag_id="sample_dag",
     default_args=default_args,
     description="A simple sample DAG",
-    schedule=timedelta(minutes=5),  # runs every 1 minute
+    schedule=timedelta(minutes=5),
     start_date=datetime(2025, 1, 1),
     catchup=False,
     tags=["example"],
 ) as dag:
 
-
-
     task1 = BashOperator(
         task_id="print_date",
         bash_command="date",
-        executor_config={"kubernetes": {"namespace": "airflow"}}
+        executor_config={
+            "pod_override": V1Pod(
+                metadata=V1ObjectMeta(namespace="airflow")
+            )
+        }
     )
 
     task2 = BashOperator(
         task_id="say_hello",
         bash_command="echo 'Hello from Airflow DAG!'",
         executor_config={
-            "pod_override": {
-                "metadata": {"namespace": "cisco"}
-            }
+            "pod_override": V1Pod(
+                metadata=V1ObjectMeta(namespace="cisco")
+            )
         }
     )
 
