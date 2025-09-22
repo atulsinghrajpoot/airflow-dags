68fb5a8538f633c9562e1e2d458a54fbd7d20901
Atul Singh Rajpoot <rajpootatul02@gmail.com>
2025-09-22 14:26:40 +0530

    Create Hello_cisco.py

---
+++ dags/Hello_cisco.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+from kubernetes.client import V1Pod, V1ObjectMeta  # import required classes
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="hellow_cisco",
+    default_args=default_args,
+    description="A simple sample DAG",
+    schedule=timedelta(minutes=5),
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    tags=["example"],
+) as dag:
+
+    task2 = BashOperator(
+        task_id="say_hello",
+        bash_command="echo 'Hello from Airflow DAG!'",
+        executor_config={
+            "pod_override": V1Pod(
+                metadata=V1ObjectMeta(namespace="cisco")
+            )
+        }
+    )
+
+    task1 >> task2
