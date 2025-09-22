13e31fd453e13a5f7e832992b0742bcb05965e6a
ENT-00182 <ent-00182@Atul.local>
2025-09-22 14:43:08 +0530

    ok

---
+++ dags/dag_simple_cisco.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from datetime import datetime, timedelta
+
+with DAG(
+    dag_id="simple_cisco_dag",
+    start_date=datetime(2025, 9, 19),
+    catchup=False,
+    schedule=timedelta(minutes=5)
+) as dag:
+
+    k8s_task = KubernetesPodOperator(
+        namespace="cisco",           # Pod will run in Cisco namespace
+        image="alpine:3.18",         # lightweight container
+        cmds=["echo", "Hello from Cisco namespace!"],
+        name="hello-cisco",
+        task_id="hello_cisco_task",
+        get_logs=True
+    )
