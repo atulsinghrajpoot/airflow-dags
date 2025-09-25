4c4bbe7bd2db7e5c9dc2177ffe8ca252b15b71dc
Atul Singh Rajpoot <rajpootatul02@gmail.com>
2025-09-25 19:11:48 +0530

    Update python_operator_test.py

--- dags/python_operator_test.py
+++ dags/python_operator_test.py
@@ -4,15 +4,6 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 from kubernetes.client import V1Pod, V1ObjectMeta  # import required classes
 
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
-}
-
 with DAG(
     dag_id="python_operator_test",
     default_args=default_args,
