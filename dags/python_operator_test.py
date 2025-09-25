0629f9b3fa0534336b617b7e3e133289f996d2ce
Atul Singh Rajpoot <rajpootatul02@gmail.com>
2025-09-25 18:48:47 +0530

    Create python_operator_test.py

---
+++ dags/python_operator_test.py
@@ -0,0 +1,34 @@
+# python_operator_test.py
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+from kubernetes.client import V1Pod, V1ObjectMeta  # import required classes
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="python_operator_test",
+    default_args=default_args,
+    description="A simple sample DAG",
+    schedule=None,
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    tags=["example"],
+) as dag:
+
+    task2 = BashOperator(
+        task_id="say_hello",
+        bash_command="echo 'Hello from Airflow DAG!'",
+        executor_config={
+            "pod_override": V1Pod(
+                metadata=V1ObjectMeta(namespace="cisco")
+            )
+        }
+    )
